53505eaaf2e4bff078d0033998a7acdfa781e84d
bnayae <Bnayanet@hotmail.com>
2019-06-29 17:10:34 +0300

    doc

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
+// https://reactjs.org/docs/hooks-reference.html
 // https://fettblog.eu/typescript-react/hooks/
 // https://www.youtube.com/watch?v=dpw9EHDh2bM&list=PLxOjILkIACgAdunnsCvfhZDbFxaAi_95Q&index=9&t=0s
 // https://medium.com/@jrwebdev/react-hooks-in-typescript-88fce7001d0d
 
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { Divider } from "@material-ui/core";
 import SimpleTexts from "./components/SimpleTexts";
--- src/components/ContextSample/ContextChildA.tsx
+++ src/components/ContextSample/ContextChildA.tsx
@@ -1,4 +1,4 @@
-// https://codesandbox.io/s/yw5zkyvx4j?from-embed
+// https://reactjs.org/docs/hooks-reference.html// https://codesandbox.io/s/yw5zkyvx4j?from-embed
 // https://upmostly.com/tutorials/how-to-use-the-usecontext-hook-in-react
 // https://daveceddia.com/usecontext-hook/
 
